13b333c3476af9a4efe2dd0cb0832314be0c88bc
4kash <kash4899@gmail.com>
2020-03-07 14:55:27 -0500

    stoopid update

---
+++ first_coding_example.py
@@ -0,0 +1,41 @@
+#math arithmetic simple operations: + - / *
+x = 5
+y = 6
+z = x + y
+print(z)
+#eponential/ power
+z = x ** 2
+print(z)
+#shortcuts +=, *= etc
+x += 5 # x = (x + 5) 
+print(x)
+'''
+for( int i = 0; i < 10 ; i++)
+{
+	System.out.print(i+1);
+}
+'''
+#for loop in python:
+for i in range(1,11):
+	print(i)
+'''
+if(something){
+	do somthing;
+}
+else if(something else)
+{
+	do something else;
+}
+'''
+x = 5 != 5
+print(x)
+# there is no else if in python, it is elif
+if 5 != 5:
+	print("yes sir")
+elif 6 != 6:
+	print("else if")
+else:
+	print("boom")
+
+#while loop
+while 
