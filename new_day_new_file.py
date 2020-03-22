a6c75f5fa0487635801bd1ca03fbed15622f3394
Peanut <ksomani@umass.edu>
2020-03-22 16:19:49 -0400

    hi

--- new_day_new_file.py
+++ new_day_new_file.py
@@ -1,11 +1,8 @@
 def pr():
 	print(5)
-def p(a, b):# inputs are parameters
-	print(a+b)
+
 pr()
 pr()
-p(5,2)
-p(1,1)
 # % division is remainder division
 if True:
 	print("yay")
@@ -14,3 +11,21 @@ def is_even(n):
 	print(n % 2 == 0)
 is_even(5)
 is_even(6)
+def p(a, b):# inputs are parameters
+	return a + b #actually gives you the value
+
+x = p(5,5)
+print(p(x,x)) 
+
+def is_prime(n):
+	if n == 2:
+		return True #once you return, method stops
+	for i in range(2,n):
+		print("for loop ran")
+		if n % i == 0:
+			return False
+	return True
+
+def print_100_primes():
+	for i in range(2,101):
+		
