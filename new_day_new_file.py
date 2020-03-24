623d5de26053d26e1b5bb6527dabc5dcc19e3fe7
Karam Sahani <karamsahani2010@gmail.com>
2020-03-24 23:40:50 +0530

    virus
    corona

--- new_day_new_file.py
+++ new_day_new_file.py
@@ -1,53 +1,22 @@
-def pr():
-	print(5)
-
-pr()
-pr()
-# % division is remainder division
-if True:
-	print("yay")
-print(5%2)
-def is_even(n):
-	print(n % 2 == 0)
-is_even(5)
-is_even(6)
-def p(a, b):# inputs are parameters
-	return a + b #actually gives you the value
-
-x = p(5,5)
-print(p(x,x)) 
-
 def is_prime(n):
-<<<<<<< HEAD
 	if n == 1:
 		return False #once you return, method stops
 	for i in range(2,n):
-=======
-	if n == 2:
-		return True #once you return, method stops
-	for i in range(2,n):
-		print("for loop ran")
->>>>>>> master
 		if n % i == 0:
 			return False
 	return True
 
-def print_100_primes():
-	for i in range(2,101):
-<<<<<<< HEAD
-		if not is_prime (i):
+def print_100_prime():
+	for j in range (1,101):
+		if is_prime(j):
+			print (j)
+print_100_prime() 
+
+def is_even(n):
+	return n % 2 == 0
+
+def print_100_even():
+	for i in range (1,101):
+		if is_even(i): 
 			print (i)
-		
-
-def square(n):
-	return n**2
-print (square (5))
-
-def square_100():
-	for i in range (0,101):
-		print (square(i))
-square_100()
-
-=======
-		
->>>>>>> master
+print_100_even()
