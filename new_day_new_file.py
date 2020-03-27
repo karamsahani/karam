e467475eb4a3fde556217121cf8c5d74947d9e65
Karam Sahani <karamsahani2010@gmail.com>
2020-03-28 00:48:49 +0530

    hi
    hello

--- new_day_new_file.py
+++ new_day_new_file.py
@@ -1,39 +1,31 @@
-def is_prime(n):
-	if n == 1:
-		return False #once you return, method stops
-	for i in range(2,n):
-		if n % i == 0:
+def is_prime(n): #made function, takes input n (n is conditional number entered by user)
+	if n == 1: #special case for 1 (1 is not prime number)
+		return False
+	i = 2 #start loop with 2
+	while (i < n): #running loop until i is less than n 
+		if n % i == 0: #check if i is a divisor of n 
+			return False #if it is divisible stop 
+		i += 1 #add 1 value to check for divisor
+	return True #after loop is over when divisor is not found print true
+print (is_prime (7)) #entering value 7 as n
+
+def pronic(n): #find if a no. is pronic number
+	i = 0
+	while(i*(i+1)<n):
+		if i*(i+1) == n: 
 			return False
+		i += 1
 	return True
+print (pronic(6))
 
-def print_100_prime():
-	for j in range (1,101):
-		if is_prime(j):
-			print (j)
-	'''
-	j=1
-	while(j<101):
-		if is_prime(j):
-			print (j)
-		j = j + 1 #j+=1
-	'''
-print_100_prime() 
+def leap_year(n):
+	if n % 4 == 0:
+		print("Its a leap year!")
+	else:
+		print("Try again!")
+print (leap_year(2988)) #why is it showing none as well?
 
-def is_even(n):
-	return n % 2 == 0
-
-def print_100_even():
-	for i in range (1,101):
-		if is_even(i): 
-			print (i)
-print_100_even()
-
-def print_first_100_primes():
-	count = 0
-	i=1
-	while(count < 100):
-		if is_prime(i):
-			print(i)
-			count+=1
-		i+=1
-print_first_100_primes()
+def math(n,m):
+	a = n + m
+	return a #how to make use of another formula under one fumction?
+print (math(8,3))
